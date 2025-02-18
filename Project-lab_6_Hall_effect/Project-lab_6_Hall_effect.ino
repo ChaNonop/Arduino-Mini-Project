cf22f39dc5d2353b97c0b257c95ef88c79c10e59
Mee lee <sabeet57@gmail.com>
2025-02-18 23:59:41 +0700

    Update Project-lab_6_Hall_effect.ino

--- Project-lab_6_Hall_effect/Project-lab_6_Hall_effect.ino
+++ Project-lab_6_Hall_effect/Project-lab_6_Hall_effect.ino
@@ -17,6 +17,9 @@ float voltage;
 float result;  //magnetic "B" (mT)
 int button;
 
+char buffer_line_1[20];
+char buffer_line_2[20];
+
 void setup() {
   Serial.begin(19200);
   Wire.begin();      
@@ -30,26 +33,37 @@ void calculate(){
   voltage = (value_adc*v_ref)/1023; 
   result = (voltage - 2.5731)/ (0.0027);  // สูตรคำนวณสนามแม่เหล็ก
 }
+void display_value(){
+  count = millis();
+  if(count - Time_display > 500) {
+    Time_display = count;
+    //printf("ADC Value: %d\n",value_adc);
+    //printf("voltage: %.2f\n",voltage);
+    //printf("Magnetic field : %.2f mT\n",result);
+    lcd.setCursor(0, 0);
+    lcd.printf(sprintf(buffer_line_1,"voltage: %.2f\n",voltage));
+    lcd.setCursor(0, 1);
+    lcd.printf(sprintf(buffer_line_2,"Magnetic field : %.2f mT\n",result));
+  }
+}
 void time_shutdown(){
   time = millis();
   if(time <= 600000){ //มากกว่า 10 นาทีให้เคลียจอ
     button = 1;
-  }
-  else{
-    button = 0;
+    display_value();
   }
 }
 
 void loop() {
   if (button = 1){ //ปุ่มเริ่มอ่านค่า
     calculate();
-    count = millis();
-    if(count - Time_display > 500) {
-      Time_display = count;
-      printf("ADC Value: %d\n",value_adc);
-      printf("voltage: %.2f\n",voltage);
-      printf("Magnetic field : %.2f mT\n",result);
-    }
     time_shutdown();
+  }
+  else{
+    button = 0;
+    lcd.setCursor(0, 0);              
+    lcd.print("Do not use ,will shutsown");
+    delay(1000);
+    lcd.clear();
   }
 }
